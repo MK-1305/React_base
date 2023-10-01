18b2a07c81f158c064fe8ef154f5a8d5c938c9f4
mk <66237794+MK-1305@users.noreply.github.com>
2023-10-01 09:46:16 +0000

    react基礎終了

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
 import React from "react";
-import ColorfulMessage from "./components/colorfulMessage";
+import { useEffect, useState } from "react/cjs/react.production.min";
+import { ColorfulMessage } from "./components/colorfulMessage";
 
 // JSX記法はタグを一つにまとめないとエラーが出るが<>でそれを回避する
 function App() {
-  const onClickButton = () => alert();
+  // [ステートの変数名、ステートを変更する関数名]を書く
+  const [num, setNum] = useState(0);
+  // ボタンを押す度に顔文字がでたり消えたりする
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
+  const onClickCountUP = () => {
+    setNum(num + 1);
+  };
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  // 最初に走らせたい処理と第二引数の値が変わった時に処理させる（関心分離）
+  useEffect(() => {
+    if (num > 0) {
+      // カウントが３の倍数の時顔文字を表示する
+      if (num % 3 === 0) {
+        // 左側がfalseなら右を返す
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        // 左側がtrueなら右を返す
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+  }, [num]);
 
   return (
     <>
@@ -15,7 +40,11 @@ function App() {
       {/* 違う書き方もできる */}
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="pink">元気です！</ColorfulMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCountUP}>カウントアップ！</button>
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
+      <p>{num}</p>
+      {/* 左側がtrueなら右を返す */}
+      {faceShowFlag && <p>(;´∀｀)</p>}
     </>
   );
 }
